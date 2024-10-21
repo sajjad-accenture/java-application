5e0f4449184853e263487cc1fb182bd7156221fc
Sajjad Khan <sajjad.c.khan@getipass.com>
2024-10-21 15:10:23 +0100

    init maven directory

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,11 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println(getGreeting());
+    }
+
+    public static String getGreeting() {
+        return "Hello, World!";
     }
 }
